d3c6e17c570d468f72e5a185adb38f9422b17047
dailiuyang <17600608814@163.com>
2018-04-21 00:33:58 +0800

    Initial commit

---
+++ src/main/java/com/daily/SpringBootsApplication.java
@@ -0,0 +1,12 @@
+package com.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootsApplication.class, args);
+	}
+}
