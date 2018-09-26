0f402c4419ee10375a70350cfd831e046793a646
json <17600608814@163.com>
2018-09-26 16:52:33 +0800

    快速排序

--- src/test/java/com/daily/jdk/sort/quickSort.java
+++ src/test/java/com/daily/jdk/sort/quickSort.java
@@ -36,8 +36,10 @@ public class quickSort {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("请初始化数组长度：");
         int n = in.nextInt();
         int[] arr = new int[n];
+        System.out.println("请初始化数组内元素：");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = in.nextInt();
         }
